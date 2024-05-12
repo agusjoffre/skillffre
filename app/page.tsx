a1a43fcbdde4dacd9154e1318714d0da1bce2756
agusjoffre <aguslj16@gmail.com>
2024-05-12 13:51:15 -0300

    init user function

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import ProjectPage from '@/components/mainpage/projects-page';
+import { initUser } from '@/lib/actions/userActions/initUser';
 
-const Home = () => {
+const Home = async () => {
+  await initUser();
+
   return (
     <main className="flex flex-col gap-10 py-16">
       <ProjectPage />
---
+++ lib/actions/userActions/initUser.ts
@@ -0,0 +1,41 @@
+'use server';
+import prisma from '@/prisma/prismaClient';
+import { currentUser } from '@clerk/nextjs/server';
+import { User } from '@prisma/client';
+import { redirect } from 'next/navigation';
+
+export const initUser = async (): Promise<User> => {
+  const user = await currentUser();
+
+  /* check if user is logged*/
+  if (!user) redirect('/register');
+  /* ---------------------------------------------------- */
+
+  /* check if user exists in db */
+  const existingUser = await prisma.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  });
+  if (existingUser) return existingUser;
+  /* ---------------------------------------------------- */
+
+  /* create new user */
+  const email = user.emailAddresses[0].emailAddress;
+  const newUser = {
+    id: user.id,
+    username: user.username ? user.username : user.fullName ? user.fullName : email,
+    email,
+    imageURL: user.imageUrl,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    isWorking: false,
+  };
+
+  const createdUser = await prisma.user.create({
+    data: newUser,
+  });
+  /* ---------------------------------------------------- */
+
+  return createdUser;
+};
