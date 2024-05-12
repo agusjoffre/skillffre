910667fb40686a7d5f2f19cf0a64dddcbebb8a6b
agusjoffre <aguslj16@gmail.com>
2024-05-12 13:30:59 -0300

    prisma modeling

---
+++ app/project/create/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const CreateProjectPage = (props: Props) => {
+  return <main className="px-8 py-10 xl:px-72">CreateProjectPage</main>;
+};
+
+export default CreateProjectPage;
